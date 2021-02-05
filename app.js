ee5f87c5ec6698bcf066a219365d196a6b2657fb
Michael Richardson <mor7991@yahoo.com>
2021-02-04 18:21:59 -0600

    finished

--- app.js
+++ app.js
@@ -8,12 +8,51 @@ let letterNum = 0;
 let fail = 0;
 let succeed = 0;
 let i = 0;
+let started = 0;
+var startTime, endTime;
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate ', 'Too ato too nOt enot one totA not anot tOO aNot ', 'oat itain oat tain nate eate tea anne inant nean ', 'itant eate anot eat nato inate eat anot tain eat ', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 $sentence.text(sentences[i]);
 
 
 $(document).keydown(logKey);
 $(document).keyup(unlogKey);
+
+function start() {
+    startTime = new Date();
+};
+
+function end() {
+    endTime = new Date();
+    let timeDiff = endTime - startTime; // in miliseconds
+    //takes away miliseconds
+    timeDiff /= 1000;
+    timeDiff /= 60;
+    //get minutes
+    let minutes = Math.round(timeDiff);
+    let formula = (54 / minutes - 2 * fail);
+    $("#target-letter").text("Your words per minute: " + formula);
+    setTimeout(function () {
+        let $div = $("<div class='row col-lg-12 text-center' id='end'></div>");
+        let $div1 = $("<div class='row col-lg-12 text-center' id='question'>Would you like to play again?</div>");
+        let $div2 = $("<div class='row text-center'></div>");
+        let $button1 = $("<button>Yes</button>");
+        let $button2 = $("<button style='padding: 0 6px'>No</button>");
+        $("#prompt-container").append($div);
+        $div.append($div1);
+        $div.append($div2);
+        $div2.append($button1);
+        $div2.append($button2);
+        $button1.click(function () {
+            location.reload();
+        })
+        $button2.click(function () {
+            $div.empty();
+            let $div3 = $("<div class='thanks'>Thanks for playing!</div>");
+            $div.append($div3);
+        })
+
+    }, 5000);
+}
 
 
 function unlogKey(e) {
@@ -31,6 +70,10 @@ function unlogKey(e) {
 
 
 function logKey(e) {
+    if (started == 0) {
+        start();
+        started++;
+    }
     if (e.keyCode == 16) {
         $lowerKeyboard.css("display", "none");
         $upperKeyboard.css("display", "block");
@@ -68,6 +111,9 @@ function logKey(e) {
             $feedback.append($span);
             letterNum++;
             fail++;
+        }
+        if (i == 4 && letterNum == sentences[i].length) {
+            end();
         }
         if (letterNum == sentences[i].length) {
             i++;
